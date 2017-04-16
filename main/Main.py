548eb7c1628ae2ee0a390bff1905aeffb94a37ab
ChangYong Lee <lichking@ChangYongui-MacBook-Pro.local>
2017-04-16 15:24:47 +0900

    exception

--- main/Main.py
+++ main/Main.py
@@ -1,4 +1,5 @@
 from first.utils.OperateUtils import *
 
+# package test
 print(sum(1, 2, 3, 4, 5))
 print(multiple(1, 2, 3, 4, 5))
---
+++ test/Exception.py
@@ -0,0 +1,34 @@
+try:
+    print("abc" + 12)
+except:
+    print("catch")
+
+try:
+    print("abc" + 12)
+except TypeError as error:
+    print(error)
+
+# multi exception
+def test(*args):
+    if len(args) > 5:
+        raise ValueError
+
+    else:
+        raise TypeError
+
+try:
+    test(1, 2)
+except ValueError as ve:
+    print(ve)
+except TypeError as te:
+    print(te)
+
+#cumtom exception
+class MyException(Exception):
+    def __str__(self):
+        return "This is Custom Exception"
+
+try:
+    raise MyException
+except MyException as me:
+    print(me)
