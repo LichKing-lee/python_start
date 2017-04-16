42aaf80f1cb43481d14f6c542d10feb1045232b6
ChangYong Lee <lichking@ChangYongui-MacBook-Pro.local>
2017-04-16 15:29:02 +0900

    import test

--- main/Main.py
+++ main/Main.py
@@ -3,3 +3,9 @@ from first.utils.OperateUtils import *
 # package test
 print(sum(1, 2, 3, 4, 5))
 print(multiple(1, 2, 3, 4, 5))
+
+import time
+
+for number in range(1, 10):
+    print(number)
+    time.sleep(2)
